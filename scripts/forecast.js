5c2de29ecb7156e56d6daa39f574325d3448c32b
wheeliemanuk <wheeliemanuk@gmail.com>
2021-06-05 18:34:03 +0100

    1st commit

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+const key = "2Jz075qUo6vxS79gWXaOTBGMKhZk74ns";
+const getWeather = async (id) => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/`;
+  const query = id + "?apikey=" + key;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+const getCity = async (city) => {
+  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const query = "?apikey=" + key + "&q=" + city;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+// getCity("hastings")
+//   .then((data) => {
+//     return getWeather(data.Key);
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err));
+// getWeather("327941");
